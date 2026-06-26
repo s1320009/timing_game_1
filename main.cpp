d6663d46957b17123a29bf7045fe17bb9028bc8a
MakotoKikuchi <anub6534@gmail.com>
2026-06-26 19:48:36 +0900

    制限時間の追加

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 
 
 #define GAME_TITLE u8"なんか動くやつ"
+#define GAME_TIME 10.0f
 
 int main(void)
 {
@@ -18,12 +19,21 @@ int main(void)
 	//circleの位置を保持する構造体
 	Ball ball = CreateBall(Vector2{200, 200}, Vector2{5, 5}, 50, RED);
 	Box box = CreateBox(300, 300, 100, 100, BLUE);
+	float gameTime = GAME_TIME; // ゲームの制限時間（秒）
 
 	while (!WindowShouldClose())
 	{
 		// ゲームの更新処理
 		UpdateBall(&ball, screenWidth, screenHeight);
 		UpdateBox(&box, ball.position, ball.radius);
+
+		if (gameTime > 0) {
+			gameTime -= GetFrameTime(); // 制限時間を減らす
+
+			if (gameTime < 0) {
+				gameTime = 0; // 制限時間が-にならないようにする
+			}
+		}
 
 		
 		// ...
@@ -33,10 +43,11 @@ int main(void)
 		DrawText("Hello, Raylib!", 190, 200, 20, LIGHTGRAY);
 		DrawBox(box);
 		DrawBall(ball);
+		DrawText(TextFormat("TIME: %.2f", gameTime), 650, 15, 25, GREEN);
 		EndDrawing();
 	}
 
-	CloseWindow();
+	CloseWindow();                    
 
 	return 0;
 }
